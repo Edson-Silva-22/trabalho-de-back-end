460935fd41fa68ac238b90f9876c745aa6d39ad0
Edson Silva <eds.17sv@gmail.com>
2023-05-20 09:54:01 -0300

    Adicionando funcionalidade de listagem de vendas

---
+++ telas/js/inicio.js
@@ -0,0 +1,43 @@
+const { createApp, ref } = Vue;
+createApp({
+    setup(){
+        const remedios = ref([
+            {
+                nome: 'Paracetamol',
+                codigo: 'RMD-001',
+                quantidade: 50
+            },
+            {
+                nome: 'Ibuprofeno',
+                codigo: 'RMD-002',
+                quantidade: 30
+            },
+            {
+                nome: 'Dipirona',
+                codigo: 'RMD-003',
+                quantidade: 20
+            },
+            {
+                nome: 'Amoxicilina',
+                codigo: 'RMD-004',
+                quantidade: 10
+            },
+            {
+                nome: 'Cetoprofeno',
+                codigo: 'RMD-005',
+                quantidade: 25
+            },
+            {
+                nome: 'Omeprazol',
+                codigo: 'RMD-006',
+                quantidade: 15
+            }
+        ]);
+
+    
+
+        return{
+            remedios,
+        }
+    }
+}).mount('#app')
