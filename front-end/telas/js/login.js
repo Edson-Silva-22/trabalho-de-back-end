85b8390cff3815c8f0c6502e66fd6f366aa8222f
Edson-Silva-22 <eds.17sv@gmail.com>
2023-07-13 09:05:45 -0300

    Alterando css da tela de login

---
+++ front-end/telas/js/login.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('butao')
+button.addEventListener('click', () => {
+    const email = document.getElementById('email')
+    const senha = document.getElementById('senha')
+    console.log('gdrgdrg');
+    /* if(email.value == null || senha.value == null){
+        alert("Ateção!!. Todos os dados devem ser preenchidos")
+    } */
+})
